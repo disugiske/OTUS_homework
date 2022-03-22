a17d1361f7c75cd7434fbbe7c8816cee4666ee5a
disugiske <disugiske@yandex.ru>
2022-03-22 15:08:52 +0300

    Add func is_prime

--- HW/homework_01/main.py
+++ HW/homework_01/main.py
@@ -10,6 +10,15 @@ power_numbers(1, 2, 5, 7)
 
 
 # filter types
+def is_prime(num):
+    f=[]
+    for i in num:
+        for a in range(2,i):
+            if i%a == 0:
+                f.append(i)
+                break
+            break
+    return list(filter(lambda x: x not in f, num))
 ODD = "odd"
 EVEN = "even"
 PRIME = "prime"
