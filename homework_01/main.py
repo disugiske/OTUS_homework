334ea9aa56d085d011876f7be77e72272cca6940
disugiske <disugiske@yandex.ru>
2022-03-23 01:26:47 +0300

    Change is_prime func - del break

--- homework_01/main.py
+++ homework_01/main.py
@@ -13,11 +13,10 @@ def power_numbers(*numbers):
 def is_prime(num):
     f=[]
     for i in num:
-        for a in range(2,i):
-            if i%a == 0:
+        for a in range(2, i):
+            if i % a == 0:
                 f.append(i)
                 break
-            break
     return list(filter(lambda x: x not in f, num))
 ODD = "odd"
 EVEN = "even"
