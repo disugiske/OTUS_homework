1805580067338d956cf18febff63d9d5a0628f5e
disugiske <disugiske@yandex.ru>
2022-03-30 22:42:35 +0300

    change except 2v

--- homework_02/exceptions.py
+++ homework_02/exceptions.py
@@ -1,9 +1,9 @@
 try:
     main()
-except LowFuelError
-    print(LowFuelError)
-except NotEnoughFuel
-    print(NotEnoughFuel)
- except CargoOverload
-    print(CargoOverload)
+except LowFuelError as e:
+    print(e)
+except NotEnoughFuel as e:
+    print(e)
+except CargoOverload as e:
+    print(e)
 
