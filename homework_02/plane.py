0cd23a275aad0146b871fe7f1d44f1875608e7c9
disugiske <disugiske@yandex.ru>
2022-03-31 01:50:26 +0300

    add remove all cargo, test pass

--- homework_02/plane.py
+++ homework_02/plane.py
@@ -3,11 +3,14 @@ from homework_02.exceptions import CargoOverload
 
 
 class Plane(Vehicle):
-    cargo = None
-    max_cargo = None
+    cargo = 0
+    max_cargo = 50
     def load_cargo(self, cargo):
         if self.cargo + cargo <= self.max_cargo:
             self.cargo += cargo
             return self.cargo
         else:
             raise CargoOverload
+    def remove_all_cargo(self):
+        cargo = 0
+        return cargo
