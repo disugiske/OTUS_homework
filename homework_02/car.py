4cb73f9dfb0cc35fb23a59a65c98afe9955e6d10
disugiske <disugiske@yandex.ru>
2022-03-30 23:21:11 +0300

    1v car

--- homework_02/car.py
+++ homework_02/car.py
@@ -1,6 +1,8 @@
 from homework_02.base import Vehicle
-class Car (Vehicle):
+class Car(Vehicle):
     engine=None
+car = Car()
     def set_engine(self, engine):
-
+        car=self.engine()
+        return car
 
