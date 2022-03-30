566ac73c35db55969b04e7c622517a880d9b3cee
disugiske <disugiske@yandex.ru>
2022-03-30 20:33:15 +0300

    make method start

--- homework_02/base.py
+++ homework_02/base.py
@@ -1,5 +1,20 @@
 from abc import ABC
-
+from homework_02.exceptions import LowFuelError, NotEnoughFuel, CargoOverload
 
 class Vehicle(ABC):
-    pass
+    started = 0
+    weight = 0
+    fuel = 0
+    fuel_consumption = 0
+    def __init__(self, weight, fuel, fuel_consumption):
+        self.weight = weight
+        self.fuel = fuel
+        self.fuel_consumption = fuel_consumption
+    def start(self,started):
+        if self.started != started:
+            if self.fuel_consumption > 0:
+                self.started = started
+            else:
+                raise LowFuelError
+    def move(self):
+
