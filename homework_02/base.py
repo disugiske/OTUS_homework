95ca1347490c9f267256b28040fc9049ab9493be
disugiske <disugiske@yandex.ru>
2022-03-31 00:14:56 +0300

    change logic start

--- homework_02/base.py
+++ homework_02/base.py
@@ -1,23 +1,26 @@
 from abc import ABC
-from homework_02.exceptions import LowFuelError
+from homework_02.exceptions import LowFuelError, NotEnoughFuel
+
 
 class Vehicle(ABC):
-    started = 1
+    started = False
     weight = 1
-    fuel = 1
+    fuel = 0
     fuel_consumption = 1
-    def __init__(self, weight, fuel, fuel_consumption, max_cargo):
+    max_cargo = 1
+    def __init__(self, weight, fuel, fuel_consumption):
         self.weight = weight
         self.fuel = fuel
         self.fuel_consumption = fuel_consumption
-        self.max_cargo = max_cargo
-    def start(self,started):
-        if self.started != started:
+    def start(self, started):
+        if started is False:
             if self.fuel_consumption > 0:
-                self.started = started
+                return started is True
             else:
                 raise LowFuelError
     def move(self, dist):
         if self.fuel_consumption * dist <= self.fuel:
             self.fuel -= self.fuel_consumption * dist
             return self.fuel
+        else:
+            raise NotEnoughFuel
