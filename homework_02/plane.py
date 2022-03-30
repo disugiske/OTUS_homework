dfc228e1f98694f20157bb13b93845c6ca0bb7ee
disugiske <disugiske@yandex.ru>
2022-03-30 22:57:07 +0300

    make load_cargo

--- homework_02/plane.py
+++ homework_02/plane.py
@@ -1,1 +1,12 @@
+from homework_02.base import Vehicle
+from homework_02.exceptions import CargoOverload
 
+
+class Plane(Vehicle):
+    cargo = None
+    max_cargo = None
+    def load_cargo(self, cargo):
+        if self.cargo + cargo <= self.max_cargo:
+            self.cargo += cargo
+        else:
+            raise CargoOverload
