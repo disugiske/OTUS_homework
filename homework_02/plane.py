f2a214c43cb3846d78fbc98c305f47e9748f5902
disugiske <disugiske@yandex.ru>
2022-04-01 21:00:02 +0300

    change back

--- homework_02/plane.py
+++ homework_02/plane.py
@@ -4,7 +4,7 @@ from homework_02.exceptions import CargoOverload
 
 class Plane(Vehicle):
     cargo = 0
-    max_cargo=100
+    max_cargo = 100
     def __init__(self, max_cargo, weight, fuel, fuel_consumption):
         super().__init__(weight, fuel, fuel_consumption)
         self.max_cargo = max_cargo
